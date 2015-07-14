6d82e6a4d4ac183bcc2d3c64f4b5e881d7d9a261
Eric Savadian <eric.j.savadian@gmail.com>
2015-07-14 05:54:44 -0400

    Set position to mark

--- lib/board.rb
+++ lib/board.rb
@@ -5,15 +5,31 @@ class Board
     Array.new(3) { Array.new(3) }
   end
 
+  def self.validate_pos(pos)
+    raise "invalid position format" unless pos.length == 2
+    raise "position out of Board range" unless pos.all? { |axis| axis >= 0 && axis <= 2 }
+  end
+
   def initialize(rows = self.class.empty_board)
     @rows = rows
   end
 
   def [](pos)
-    raise "invalid position format" unless pos.length == 2
-    raise "position out of Board range" unless pos.all? { |axis| axis >= 0 && axis <= 2 }
+    self.class.validate_pos(pos)
 
     row, col = pos[0], pos[1]
     @rows[row][col]
+  end
+
+  def []=(pos, mark)
+    self.class.validate_pos(pos)
+    raise "mark already placed at position" unless empty?(pos)
+    
+    row, col = pos[0], pos[1]
+    @rows[row][col] = mark
+  end
+
+  def empty?(pos)
+    self[pos].nil?
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,12 +23,30 @@ describe Board do
       expect( sample_board[[2,2]] ).to eq(nil)
     end
 
+    it "sets the mark at a coordinate" do
+      sample_board[[1,0]] = :x
+      sample_board[[1,2]] = :o
+      sample_board[[2,2]] = :x
+
+      expect( sample_board[[1,0]] ).to eq(:x)
+      expect( sample_board[[1,2]] ).to eq(:o)
+      expect( sample_board[[2,2]] ).to eq(:x)
+    end
+
     it "raises an error on invalid positions" do
       expect { sample_board[[1]] }.to raise_error("invalid position format")
       expect { sample_board[[1,2,3]] }.to raise_error("invalid position format")
       expect { sample_board[[0,3]] }.to raise_error("position out of Board range")
       expect { sample_board[[3,0]] }.to raise_error("position out of Board range")
       expect { sample_board[[-3, 1]] }.to raise_error("position out of Board range")
+
+      expect { sample_board[[1]] = :x }.to raise_error("invalid position format")
+      expect { sample_board[[1,2,3]] = :o }.to raise_error("invalid position format")
+      expect { sample_board[[0,3]] =  :x }.to raise_error("position out of Board range")
+      expect { sample_board[[3,0]] = :o }.to raise_error("position out of Board range")
+      expect { sample_board[[-3, 1]] = nil }.to raise_error("position out of Board range")
+
+      expect { sample_board[[0,0]] = :o }.to raise_error("mark already placed at position")
     end
   end
 end
