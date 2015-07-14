97fa657692304f9cc9eea57dace28c48a7fb2ad1
Eric Savadian <eric.j.savadian@gmail.com>
2015-07-14 05:41:55 -0400

    Return valid board positions

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 class Board
-  attr_accessor :rows
+  attr_reader :rows
 
   def self.empty_board
     Array.new(3) { Array.new(3) }
@@ -7,5 +7,13 @@ class Board
 
   def initialize(rows = self.class.empty_board)
     @rows = rows
+  end
+
+  def [](pos)
+    raise "invalid position format" unless pos.length == 2
+    raise "position out of Board range" unless pos.all? { |axis| axis >= 0 && axis <= 2 }
+
+    row, col = pos[0], pos[1]
+    @rows[row][col]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,34 @@
 require 'board'
 
 describe Board do
+  let(:sample_board) do
+    Board.new([[:x, :o, :x],
+               [nil, :o, nil],
+               [nil, :x, nil]])
+  end
+  
   describe '#initialize' do
     it "initalizes a new empty 3x3 board" do
       empty_board = Board.new
-      expect(empty_board.rows.count).to eq(3)
-      expect(empty_board.rows.flatten.count).to eq(9)
-      expect(empty_board.rows.flatten.compact).to eq([])
+      expect( empty_board.rows.count ).to eq(3)
+      expect( empty_board.rows.flatten.count ).to eq(9)
+      expect( empty_board.rows.flatten.compact ).to eq([])
+    end
+  end
+
+  context 'working with positions' do
+    it "returns the mark at a coordinate" do
+      expect( sample_board[[0,0]] ).to eq(:x)
+      expect( sample_board[[1,1]] ).to eq(:o)
+      expect( sample_board[[2,2]] ).to eq(nil)
+    end
+
+    it "raises an error on invalid positions" do
+      expect { sample_board[[1]] }.to raise_error("invalid position format")
+      expect { sample_board[[1,2,3]] }.to raise_error("invalid position format")
+      expect { sample_board[[0,3]] }.to raise_error("position out of Board range")
+      expect { sample_board[[3,0]] }.to raise_error("position out of Board range")
+      expect { sample_board[[-3, 1]] }.to raise_error("position out of Board range")
     end
   end
 end
