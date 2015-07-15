909e26e7a8ff27b438254059bb66dbdc9d1ca48b
Eric Savadian <eric.j.savadian@gmail.com>
2015-07-14 23:18:21 -0400

    Return a Game Node's Children

--- lib/game_node.rb
+++ lib/game_node.rb
@@ -5,4 +5,24 @@ class TicTacToeNode
     @board, @next_mark, @prev_mark_pos =
       board, next_mark, prev_mark_pos
   end
+
+  def children
+    row_max, col_max = @board.rows.length - 1, @board.rows.first.length - 1
+    
+    [].tap do |children|
+      (0..row_max).each do |row_idx|
+        (0..col_max).each do |col_idx|
+          pos = [row_idx, col_idx]
+
+          next unless board.empty_pos?(pos)
+
+          new_board = board.dup
+          new_board[pos] = self.next_mark
+          next_mark = board.marks.rotate.first
+
+          children << TicTacToeNode.new(new_board, next_mark, pos)
+        end
+      end
+    end
+  end
 end
--- spec/game_node_spec.rb
+++ spec/game_node_spec.rb
@@ -5,6 +5,13 @@ describe TicTacToeNode do
     TicTacToeNode.new(Board.new, :x)
   end
 
+  let(:first_child_node) do
+    TicTacToeNode.new(Board.new({ rows: [[:x, nil, nil],
+                                        [nil, nil, nil],
+                                        [nil, nil, nil]]}),
+                      :o, [0, 0])
+  end
+
   describe '#initialize' do
     it "sets up the node" do
       board = Board.new
@@ -12,6 +19,13 @@ describe TicTacToeNode do
       node = TicTacToeNode.new(board, mark)
       expect( node.board ).to eq(board)
       expect( node.next_mark ).to eq(mark)
+    end
+  end
+
+  describe '#children' do
+    it "generates as many children as there are empty spaces" do
+      expect(empty_node.children.length).to eq(9)
+      expect(first_child_node.children.length).to eq(8)
     end
   end
 end
