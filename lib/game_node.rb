e29eb0843f4fd82ff54ed640402d4a8934e4f829
Eric Savadian <eric.j.savadian@gmail.com>
2015-07-15 00:06:14 -0400

    Check losing state of child nodes

--- lib/game_node.rb
+++ lib/game_node.rb
@@ -6,9 +6,27 @@ class TicTacToeNode
       board, next_mark, prev_mark_pos
   end
 
+  def losing_node?(mark)
+    if board.over?
+      return board.won? && (board.winner != mark)
+    end
+
+    if @next_mark == mark
+      self.children.all? { |child| child.losing_node?(mark) }
+    else
+      self.children.any? { |child| child.losing_node?(mark) }
+    end
+  end
+
+  def winning_node?(mark)
+    if board.over?
+      return board.winner == mark
+    end
+  end
+
   def children
     row_max, col_max = @board.rows.length - 1, @board.rows.first.length - 1
-    
+
     [].tap do |children|
       (0..row_max).each do |row_idx|
         (0..col_max).each do |col_idx|
--- spec/game_node_spec.rb
+++ spec/game_node_spec.rb
@@ -24,32 +24,71 @@ describe TicTacToeNode do
 
   describe '#children' do
     it "generates as many children as there are empty spaces" do
-      expect(empty_node.children.length).to eq(9)
-      expect(first_child_node.children.length).to eq(8)
+      expect( empty_node.children.length ).to eq(9)
+      expect( first_child_node.children.length ).to eq(8)
     end
 
     it "children have different marks than their parent" do
-      expect(empty_node.children.all? do |child|
+      expect( empty_node.children.all? do |child|
         child.next_mark == :o
-      end).to eq(true)
+      end ).to eq(true)
     end
 
     it "children's prev_mark_pos equal to parent's move" do
       child_prev_moves = empty_node.children.map{ |child| child.prev_mark_pos }
-      expect(child_prev_moves).to match_array([0,1,2].product([0,1,2]))
+      expect( child_prev_moves ).to match_array([0,1,2].product([0,1,2]))
     end
 
     it "children's boards are dup of parent's" do
       child_boards = empty_node.children.map { |child| child.board }
-      expect(child_boards.none? do |child_board|
+      expect( child_boards.none? do |child_board|
         child_board.object_id == empty_node.board.object_id
-      end).to eq(true)
+      end ).to eq(true)
     end
   end
 
   describe '#winning_node?' do
+
   end
 
   describe '#losing_node?' do
+    it "knows when a node is in a losing state" do
+      empty_node.board[[0,0]] = :o
+      empty_node.board[[0,1]] = :o
+      empty_node.board[[0,2]] = :o
+      expect( empty_node.losing_node?(:o) ).to eq(false)
+      expect( empty_node.losing_node?(:x) ).to eq(true)
+    end
+
+    let(:loser) do
+      node = TicTacToeNode.new(Board.new, :x)
+      node.board[[0,0]] = :o
+      node.board[[0,2]] = :o
+      node.board[[2,2]] = :o
+      node
+    end
+
+    let(:opponent_wins) do
+      node = TicTacToeNode.new(Board.new, :o)
+      node.board[[0, 0]] = :x
+      node.board[[0, 1]] = :x
+      node.board[[0, 2]] = :o
+      node.board[[1, 1]] = :o
+      node.board[[1, 0]] = :x
+      node
+    end
+
+    context "on the player's turn" do
+      it "knows when every child is in losing state" do
+        expect( loser.losing_node?(:x) ).to eq(true)
+      end
+    end
+
+    context "on the opponent's turn" do
+      it "knows when every child is in losing state" do
+        expect( loser.losing_node?(:o) ).to eq(false)
+        expect( opponent_wins.losing_node?(:x) ).to eq(true)
+      end
+    end
   end
 end
